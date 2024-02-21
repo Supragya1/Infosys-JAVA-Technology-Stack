fc7fa7a6ea0e84de76a7335e52852ddaafe58940
Supragya1 <ssg.gandotra@gmail.com>
2024-02-21 23:56:32 +0530

    Assignment 5

---
+++ Programming-using-java/Control-Structure-5/Assignments/Assignment_5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Assignment_5 {
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter your account number");
+	long account_number = scan.nextLong();
+	System.out.println("Enter your account balance");
+	long account_balance = scan.nextLong();
+	System.out.println("Enter your salary");
+    long salary = scan.nextLong();
+    System.out.println("Enter the loan type");
+    String loan_type = scan.nextLine();
+    System.out.println("Enter the expected loan amount?");
+    long expected_loan_amount = scan.nextLong();
+    System.out.println("Enter the expected emis");
+    long expected_emi = scan.nextLong();
+    if(account_number ==5){
+
+    }
+    if(loan_type.toLowerCase()=="car" &&){
+    }
+	}
+}
+
