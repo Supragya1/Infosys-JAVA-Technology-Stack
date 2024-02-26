96f1b5920cd66acd0e53790613de969e572955ab
Supragya1 <ssg.gandotra@gmail.com>
2024-02-26 07:45:36 +0530

    Assignment 8

---
+++ Programming-using-java/Control-Structure-5/Assignments/Iteration-Control-Assignments/Assignment_8.java
@@ -0,0 +1,22 @@
+/**
+ * Implement a program to display the below pattern.
+*****
+****
+***
+**
+*
+ */
+import java.util.Scanner;
+public class Assignment_8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int n = scan.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
