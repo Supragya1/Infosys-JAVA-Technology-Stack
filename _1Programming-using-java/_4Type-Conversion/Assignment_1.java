b10362d3b70968555acdaf3eb92e8bc16ebd388d
Supragya1 <ssg.gandotra@gmail.com>
2024-02-27 23:38:38 +0530

    Assignment 1

---
+++ _1Programming-using-java/_4Type-Conversion/Assignment_1.java
@@ -0,0 +1,18 @@
+/**
+ * Implement a program to find the area of a circle by using the formula given below and display the calculated area. 
+ * Area = pi*radius*radius 
+ * The value of pi is 3.14.
+ * Sample Input             Expected Output
+ * radius = 5               Area of the circle = 78.5
+ * radius = 10              Area of the circle = 314.0
+ */
+import java.util.Scanner;
+public class Assignment_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of the circle = " + area);
+    }
+}
