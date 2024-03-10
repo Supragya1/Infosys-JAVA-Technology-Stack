781f02b25dd1637fbb40ff848cc3edd67c07806d
Supragya1 <ssg.gandotra@gmail.com>
2024-03-10 23:37:31 +0530

    Exercise

---
+++ _1Programming-using-java/_15Arrays/Exercise.java
@@ -0,0 +1,23 @@
+/**
+ * Calculate and return the sum of all the even numbers present in the numbers array passed to the 
+ * method calculateSumOfEvenNumbers. Implement the logic inside calculateSumOfEvenNumbers() method.
+ * Test the functionalities using the main() method of the Tester class.
+ *      Sample Input                Expected Output
+ * {68,79,86,99,23,2,41,100}              256
+ * {1,2,3,4,5,6,7,8,9,10}                 30
+ */
+public class Exercise {
+    public static int calculateSumOfEvenNumbers(int[] numbers){
+        int sum=0;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]%2==0){
+                sum+=numbers[i];
+            }
+        }
+        return sum;
+    }
+	public static void main(String[] args) {
+		int[] numbers = {68,79,86,99,23,2,41,100};
+		System.out.println("Sum of even numbers: " +calculateSumOfEvenNumbers(numbers));
+	}
+}
