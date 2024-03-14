b709598bf9df9781e01c8244aab28bccb05d826d
Supragya1 <ssg.gandotra@gmail.com>
2024-03-15 00:01:11 +0530

    Assignment 1

--- _1Programming-using-java/_14String/Assignments/Assignment_1.java
+++ _1Programming-using-java/_14String/Assignments/Assignment_1.java
@@ -1,3 +1,14 @@
+/**
+ * Complete the moveSpecialCharacters() method given in the Tester class.
+ * Method Description
+ * moveSpecialCharacters(String str)
+ * Move all the special characters present in the string passed to the method to the end of the string and return the modified string.
+ * Note: Assume that the input string does not have any space.
+ * Test the functionalities using the main() method of the Tester class. 
+ * Sample Input                 Expected Output
+ * He@#$llo!*&                  Hello@#$!*&
+ * %$Wel*&come!                Welcome%$!*&
+ */
 public class Assignment_1{
     public static String moveSpecialCharacters(String str){
         StringBuilder letter = new StringBuilder();
