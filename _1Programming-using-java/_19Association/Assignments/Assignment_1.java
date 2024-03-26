cf94cd59cc1eddd0258e6e5934bdc1fe125df139
Supragya1 <ssg.gandotra@gmail.com>
2024-03-26 23:54:58 +0530

    Assignment 1

---
+++ _1Programming-using-java/_19Association/Assignments/Assignment_1.java
@@ -0,0 +1,17 @@
+/**
+ * A dance club is conducting auditions to admit interested candidates. You need to implement a 
+ * Participant class for the dance club based on the class diagram and description given below.
+ * Method Description
+ * Partcipant(String name, long contactNumber, String city)
+ * Initialize the name, contactNumber and city instance variables appropriately with the values passed to the constructor.
+ * Generate the registrationId using the static variable counter. The value of registrationId should start from 'D10001' and the numerical part should be incremented by 1 for the subsequent values. Initialize the counter in static block.
+ * Implement the appropriate getter and setter methods.
+ * Test the functionalities using the provided Tester class. Create two or more Participant objects and validate that the values of the member variables are proper.
+ * Sample Input and Output
+ * For constructor
+ * Input
+ * For first Participant object
+ */
+public class Assignment_1 {
+    
+}
