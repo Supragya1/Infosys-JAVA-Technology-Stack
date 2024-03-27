36ebbbee3df4b98808b8359d9391d966c2dc18f1
Supragya1 <ssg.gandotra@gmail.com>
2024-03-27 23:46:31 +0530

    Assignment 1

--- _1Programming-using-java/_19Association/Assignments/Assignment_1.java
+++ _1Programming-using-java/_19Association/Assignments/Assignment_1.java
@@ -12,6 +12,46 @@
  * Input
  * For first Participant object
  */
+class Participant{
+    private String name;
+    private long contactNumber;
+    private String city;
+    private String registrationId;
+    private static int counter = 10000;
+    static{
+        counter++;
+    }
+    public Participant(String name, long contactNumber, String city){
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.city = city;
+        this.registrationId = "D" + counter;
+    }
+    public String getName(){
+        return name;
+    }
+    public long getContactNumber(){
+        return contactNumber;
+    }
+    public String getCity(){
+        return city;
+    }
+    public String getRegistrationId(){
+        return registrationId;
+    }
+
+}
 public class Assignment_1 {
-    
+    public static void main(String[] args) {
+        Participant p1 = new Participant("John", 9876543210L, "Bangalore");
+        System.out.println("Name: " + p1.getName());
+        System.out.println("Contact Number: " + p1.getContactNumber());
+        System.out.println("City: " + p1.getCity());
+        System.out.println("Registration Id: " + p1.getRegistrationId());
+        Participant p2 = new Participant("Jane", 9876543211L, "Chennai");
+        System.out.println("Name: " + p2.getName());
+        System.out.println("Contact Number: " + p2.getContactNumber());
+        System.out.println("City: " + p2.getCity());
+        System.out.println("Registration Id: " + p2.getRegistrationId());
+    }
 }
